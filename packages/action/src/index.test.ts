87e2d0d99f3215cd5183eed0dc886f7b02793689
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-07-19 23:02:32 +0300

    feat(*): ActionBuilder + Action

---
+++ packages/action/src/index.test.ts
@@ -0,0 +1,83 @@
+import { Action, ActionBuilder } from "./index"
+
+describe("Action", () => {
+	test("action saves promise for stage if not rejected", async () => {
+		const simple = generateSimpleAction<number>()
+
+		const promise1 = simple.action.run(0)
+		expect(promise1).toBe(simple.action.run(0))
+
+		simple.promise.resolve(10)
+		expect(await promise1).toEqual(10)
+
+		expect(promise1).toBe(simple.action.run(0))
+	})
+
+	test("action doesn't save promise for stage if rejected", async () => {
+		const simple = generateSimpleAction<number>()
+
+		const promise1 = simple.action.run(0)
+		expect(promise1).toBe(simple.action.run(0))
+
+		simple.promise.reject(new Error("rejected"))
+		await expect(async () => await promise1).rejects.toThrow(new Error("rejected"))
+
+		expect(simple.action.run(0)).not.toBe(promise1)
+	})
+
+	test("action returns result", async () => {
+		const simple = generateSimpleAction<number>()
+
+		simple.action.run(0).then()
+		simple.promise.resolve(10)
+
+		expect(await simple.action.result).toEqual(10)
+	})
+
+	test("action works for some stages", async () => {
+		const promise1 = generatePromise<number>()
+
+		const action = ActionBuilder.create<string>()
+			.then({ id: "s1", run: () => promise1.promise() })
+			.then({ id: "s2", run: async value => `str-${value}` })
+			.build()
+
+		expect(() => action.run(1)).toThrowError(new Error("Stage 0 hasn't been run yet"))
+		const s1 = action.run(0)
+		expect(() => action.run(1)).toThrowError(new Error("Stage 0 status is: pending"))
+		promise1.resolve(10)
+		expect(await s1).toBe(10)
+
+		const s2 = action.run(1)
+		expect(await s2).toBe("str-10")
+		expect(await action.result).toBe("str-10")
+		expect(s2).toBe(action.result)
+	})
+})
+
+function generateSimpleAction<T>() {
+	const promise = generatePromise<T>()
+	return {
+		promise,
+		action: ActionBuilder.create<string>()
+			.then({ id: "one", run: () => promise.promise() })
+			.build(),
+	}
+}
+
+function generatePromise<T>() {
+	const result: {
+		resolve: (value: T) => void
+		reject: (err: any) => void
+		promise: () => Promise<T>
+	} = {
+		resolve: () => null,
+		reject: () => null,
+		promise: () =>
+			new Promise<T>((resolve, reject) => {
+				result.resolve = resolve
+				result.reject = reject
+			}),
+	}
+	return result
+}
---
+++ packages/action/src/index.ts
@@ -0,0 +1,88 @@
+type Arr = readonly unknown[]
+
+type LastItemType<T> = T extends [...Arr, infer Last] ? Last : void
+
+type Stage<Id, T, R> = {
+	id: Id
+	run: (value: T) => Promise<R>
+}
+
+type PromiseState<T> = { status: "pending" } | { status: "rejected"; error: any } | { status: "resolved"; value: T }
+
+export class Action<Id, Rs extends Arr> {
+	private readonly state: PromiseState<unknown>[]
+	private readonly promises: Promise<unknown>[]
+
+	constructor(readonly stages: Stage<Id, unknown, unknown>[]) {
+		// @ts-ignore
+		this.state = new Array(stages.length)
+		this.promises = new Array(stages.length)
+	}
+
+	get ids(): Id[] {
+		return this.stages.map(s => s.id)
+	}
+
+	run<T extends keyof Rs & number>(idx: T): Promise<Rs[T]> {
+		if (idx === 0) {
+			return this.runInternal(idx, null)
+		}
+		if (idx >= this.stages.length) {
+			throw new Error(`Stage with index ${idx} not found`)
+		}
+		const prevState = this.state[idx - 1]
+		if (prevState == null) {
+			throw new Error(`Stage ${idx - 1} hasn't been run yet`)
+		}
+		if (prevState.status !== "resolved") {
+			throw new Error(`Stage ${idx - 1} status is: ${prevState.status}`)
+		}
+		return this.runInternal(idx, prevState.value)
+	}
+
+	private runInternal<T extends keyof Rs & number>(idx: T, input: any): Promise<Rs[T]> {
+		// @ts-ignore
+		const state: PromiseState<Rs[T]> = this.state[idx]
+		if (state != null && (state.status === "pending" || state.status === "resolved")) {
+			return this.promises[idx]
+		} else {
+			this.state[idx] = { status: "pending" }
+			const promise = this.stages[idx].run(input)
+			this.promises[idx] = promise
+			promise
+				.then(r => {
+					this.state[idx] = { status: "resolved", value: r }
+					return r
+				})
+				.catch(err => {
+					this.state[idx] = { status: "rejected", error: err }
+					return Promise.reject(err)
+				})
+			return promise
+		}
+	}
+
+	get result(): Promise<LastItemType<Rs>> {
+		// @ts-ignore
+		return this.run(this.stages.length - 1)
+	}
+}
+
+export class ActionBuilder<Id, Rs extends Arr> {
+	constructor(private readonly stages: Stage<Id, unknown, unknown>[]) {}
+
+	then<T>(stage: Stage<Id, LastItemType<Rs>, T>): ActionBuilder<Id, [...Rs, T]> {
+		// @ts-ignore
+		return new ActionBuilder([...this.stages, stage])
+	}
+
+	build(): Action<Id, Rs> {
+		// @ts-ignore
+		return new Action(this.stages)
+	}
+
+	static create<Id>() {
+		// @ts-ignore
+		return new ActionBuilder([]) as ActionBuilder<Id, []>
+	}
+}
