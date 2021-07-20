b91ab48c1d385dc8464b7e31d5da8d2c513a57ef
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-07-20 11:25:18 +0300

    fix(*): minor code fixes

--- packages/test-provider/src/index.ts
+++ packages/test-provider/src/index.ts
@@ -13,6 +13,7 @@ export class TestSubprovider extends HookedWalletSubprovider {
 			getAccounts: function (cb: any) {
 				cb(null, [wallet.getAddressString()])
 			},
+
 			signTransaction: function (txData: any, cb: any) {
 				// defaults
 				if (txData.gas !== undefined) txData.gasLimit = txData.gas
@@ -23,11 +24,13 @@ export class TestSubprovider extends HookedWalletSubprovider {
 				tx.sign(wallet.getPrivateKey())
 				cb(null, "0x" + tx.serialize().toString("hex"))
 			},
+
 			signPersonalMessage: function (msgParams: any, cb: any) {
 				const privateKey = wallet.getPrivateKey()
 				const serialized = personalSign(privateKey, msgParams)
 				cb(null, serialized)
 			},
+
 			signTypedMessage: function (msgParams: any, cb: any) {
 				const privateKey = wallet.getPrivateKey()
 				const sig = signTypedData_v4(privateKey, { data: msgParams.data })
