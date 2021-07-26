6ba1558403e1d850f0f23df251766f359a5e301c
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-07-26 22:23:44 +0300

    fix(*): minor

--- packages/test-provider/src/index.ts
+++ packages/test-provider/src/index.ts
@@ -10,6 +10,7 @@ export type TestProviderChain = {
 	chainId: number
 }
 
+// noinspection JSUnusedGlobalSymbols
 export class TestSubprovider extends HookedWalletSubprovider {
 	constructor(wallet: Wallet, chain?: TestProviderChain) {
 		const privateKey = wallet.getPrivateKey()
