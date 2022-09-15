bcbbca46c325cee7e479f66a07f98be4a610d81f
Aleksandr Sozinov <a.sozinov@rarible.com>
2022-09-15 20:17:58 +0300

    feat: network error helpers

--- packages/logger/src/errors.ts
+++ packages/logger/src/errors.ts
@@ -1,3 +1,5 @@
+import type {RequestInit} from "node-fetch"
+
 // eslint-disable-next-line unicorn/custom-error-definition
 export class Warning extends Error {
 	constructor(message: string) {
@@ -8,16 +10,20 @@ export class Warning extends Error {
 }
 
 export class NetworkError extends Error {
-	status: number
+	status?: number
 	url: string
-	value: any
+	data?: any
 	code?: string
+	formData?: any
+	method?: string
 
 	constructor(options: {
-		status: number,
+		status?: number,
 		url: string,
-		value: any,
-		code?: string
+		data?: any,
+		code?: string,
+		formData?: any,
+		method?: string
 	}) {
 		super(JSON.stringify(options, null, " "))
 		Object.setPrototypeOf(this, NetworkError.prototype)
@@ -25,6 +31,43 @@ export class NetworkError extends Error {
 		this.code = options.code || "NETWORK_ERR"
 		this.status = options.status
 		this.url = options.url
-		this.value = options.value
+		this.data = options.data
+		this.formData = options.formData
+		this.method = options.method
+	}
+}
+
+export async function handleFetchErrorResponse(fetchResponse: any, options?: {
+	code?: string,
+	requestInit?: RequestInit
+}) {
+	if (fetchResponse && !fetchResponse.ok) {
+		let responseData
+		try {
+			responseData = await fetchResponse.clone().json()
+		} catch (e) {
+			responseData = await fetchResponse.clone().text()
+		}
+		throw new NetworkError({
+			status: fetchResponse.status,
+			url: fetchResponse.url,
+			data: responseData,
+			formData: options?.requestInit?.body?.toString(),
+			method: options?.requestInit?.method,
+			code: options?.code,
+		})
+	}
+}
+
+export function handleAxiosErrorResponse(axiosError: any, options?: { code: string }) {
+	if (axiosError?.isAxiosError === true && axiosError.response) {
+		throw new NetworkError({
+			status: axiosError?.response?.status,
+			url: axiosError?.config?.url,
+			data: axiosError?.response?.data,
+			formData: axiosError?.config?.data,
+			method: axiosError?.config?.method,
+			code: options?.code,
+		})
 	}
 }
