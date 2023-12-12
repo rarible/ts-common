1b5b24df9f1c15306aa2ebb81b91a894d76d667e
Ivan Koryakovtsev <ivan@rarible.com>
2023-12-12 12:41:14 +0000

    feat: migrate BlockchainEnum to union types

--- packages/types/src/blockchains/union/enum/index.ts
+++ packages/types/src/blockchains/union/enum/index.ts
@@ -1,3 +1,22 @@
+import { CustomError } from "@rarible/utils/build/error/custom"
+
+export const L1Blockchains = ["ETHEREUM", "FLOW", "TEZOS", "SOLANA"] as const
+export type L1Blockchain = (typeof L1Blockchains)[number]
+
+/**
+ * @depreacted
+ * please use L1Blockchains instead
+ * it will be removed in 0.11.0
+ */
+
+export const blockchainsLayer1 = L1Blockchains
+
+/**
+ * @deprecated
+ * please use literals instead of enums e.g. "ETHEREUM" instead of BlockchainLayer1Enum.ETHEREUM
+ * it will be removed in 0.11.0
+ */
+
 export enum BlockchainLayer1Enum {
   ETHEREUM = "ETHEREUM",
   FLOW = "FLOW",
@@ -5,7 +24,28 @@ export enum BlockchainLayer1Enum {
   SOLANA = "SOLANA",
 }
 
-export const blockchainsLayer1 = Object.values(BlockchainLayer1Enum)
+export const evmBlockchains = ["ETHEREUM", "POLYGON", "MANTLE"] as const
+export type EVMBlockchain = (typeof evmBlockchains)[number]
+
+export function isEVMBlockchain(blockchain: Blockchain): blockchain is EVMBlockchain {
+  return evmBlockchains.includes(blockchain as EVMBlockchain)
+}
+
+export const evmLikeBlockchains = [...evmBlockchains, "IMMUTABLEX"] as const
+export type EVMLikeBlockchain = (typeof evmLikeBlockchains)[number]
+
+export function isEVMLikeBlockchain(blockchain: Blockchain): blockchain is EVMLikeBlockchain {
+  return evmLikeBlockchains.includes(blockchain as EVMLikeBlockchain)
+}
+
+export const blockchains = [...evmLikeBlockchains, "FLOW", "TEZOS", "SOLANA"] as const
+export type Blockchain = (typeof blockchains)[number]
+
+/**
+ * @deprecated
+ * please use literals instead of enums e.g. "POLYGON" instead of BlockchainEnum.POLYGON
+ * it will be removed in 0.11.0
+ */
 
 export enum BlockchainEnum {
   ETHEREUM = "ETHEREUM",
@@ -17,71 +57,80 @@ export enum BlockchainEnum {
   MANTLE = "MANTLE",
 }
 
-export const evmBlockchains = [
-  BlockchainEnum.POLYGON,
-  BlockchainEnum.IMMUTABLEX,
-  BlockchainEnum.POLYGON,
-  BlockchainEnum.MANTLE,
-]
-export type EVMBlockchain = (typeof evmBlockchains)[number]
-
-export function isEVMBlockchain(blockchain: BlockchainEnum): blockchain is EVMBlockchain {
-  return evmBlockchains.includes(blockchain)
-}
-
-export const blockchains = Object.values(BlockchainEnum)
-
-export function isBlockchainSpecified(value: string): value is WithLayer1Blockchain {
-  for (const blockchain of blockchainsLayer1) {
-    if (value.startsWith(`${blockchain}:`)) {
-      return true
-    }
+export function isWithL1Blockchain(value: string): value is WithLayer1Blockchain {
+  for (const blockchain of L1Blockchains) {
+    if (value.startsWith(`${blockchain}:`)) return true
   }
   return false
 }
 
-export function isRealBlockchainSpecified(value: string): value is WithBlockchain {
+/**
+ * @deprecated
+ * please use `isWithL1Blockchain` instead of `isBlockchainSpecified`
+ */
+
+export const isBlockchainSpecified = isWithL1Blockchain
+
+export function isWithBlockchain(value: string): value is WithBlockchain {
   for (const blockchain of blockchains) {
-    if (value.startsWith(`${blockchain}:`)) {
-      return true
-    }
+    if (value.startsWith(`${blockchain}:`)) return true
   }
   return false
 }
 
-export type WithLayer1Blockchain = string & {
+/**
+ * @deprecated
+ * please use `isWithBlockchain` instead of `isRealBlockchainSpecified`
+ */
+
+export const isRealBlockchainSpecified = isWithBlockchain
+
+export type WithLayer1Blockchain<T extends L1Blockchain = L1Blockchain> = `${T}:${string}` & {
   __WITH_BLOCKCHAIN__: true
 }
 
-export function withLayer1Blockchain(blockchain: BlockchainLayer1Enum, raw: string): WithLayer1Blockchain {
+export function withL1Blockchain(blockchain: L1Blockchain, raw: string): WithLayer1Blockchain {
   return `${blockchain}:${raw}` as WithLayer1Blockchain
 }
 
-export type WithBlockchain = string & {
+/**
+ * @deprecated
+ * please use `withL1Blockchain` instead of `withLayer1Blockchain`
+ */
+
+export const withLayer1Blockchain = withL1Blockchain
+
+export type WithBlockchain<T extends Blockchain = Blockchain> = `${T}:${string}` & {
   __WITH_BLOCKCHAIN__: true
 }
 
-export function withBlockchain(blockchain: BlockchainEnum, raw: string): WithBlockchain {
-  return `${blockchain}:${raw}` as WithBlockchain
+export function withBlockchain<T extends Blockchain>(blockchain: T, raw: string): WithBlockchain<T> {
+  return `${blockchain}:${raw}` as WithBlockchain<T>
 }
 
-export function toLayerOneBlockchain(blockchain: BlockchainEnum): BlockchainLayer1Enum {
-  if (isEVMBlockchain(blockchain)) return BlockchainLayer1Enum.ETHEREUM
+export function toL1Blockchain(blockchain: Blockchain): L1Blockchain {
+  if (isEVMLikeBlockchain(blockchain)) return "ETHEREUM"
   return blockchain
 }
 
-export function parseBlockchain(value: WithBlockchain): [BlockchainEnum, string] {
-  if (isRealBlockchainSpecified(value)) {
+/**
+ * @deprecated
+ * please use `toL1Blockchain` instead of `toLayerOneBlockchain`
+ */
+
+export const toLayerOneBlockchain = toL1Blockchain
+
+export function parseBlockchain(value: WithBlockchain): [Blockchain, string] {
+  if (isWithBlockchain(value)) {
     const sliceIndex = value.indexOf(":")
     const blockchain = value.substring(0, sliceIndex)
     const rest = value.substring(sliceIndex + 1)
-    return [blockchain as BlockchainEnum, rest]
+    return [blockchain as Blockchain, rest]
   }
   throw new BlockchainParseError(value)
 }
 
-export class BlockchainParseError extends Error {
-  readonly name = "BlockchainParseError"
+export class BlockchainParseError extends CustomError {
   constructor(value: string) {
     super(`Can't parse blockchain from value ${value}`)
   }
