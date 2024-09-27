65bc81b81d5068ac9868d0d9e462d0dfcf527560
Aleksandr Sozinov <sanzeting@mail.ru>
2024-09-27 16:37:03 +0100

    fix: cross packages imports (#70)
    Co-authored-by: Aleksandr Sozinov <a.sozinov@rarible.com>

--- packages/types/src/blockchains/common/contract-address.ts
+++ packages/types/src/blockchains/common/contract-address.ts
@@ -1,4 +1,4 @@
-import { CustomError } from "@rarible/utils/error/custom/index"
+import { CustomError } from "@rarible/utils"
 import type { BlockchainEnum, BlockchainLayer1Enum, L1BlockchainByBlockchain } from "../union/enum/domain.js"
 import type { AbstractAddress } from "./address.js"
 
